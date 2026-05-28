9a77db3af7bf087a27e7c8886974afad4532d40c
XingXiaoWu <XingXiaoWu@users.noreply.github.com>
2026-05-28 04:26:59 +0000

    deploy: bb5c2e6d93f8db8c878b07c3184bbbfd1f3df828

--- assets/index-DFAVbgdu.js
+++ assets/index-DFAVbgdu.js
@@ -2912,18 +2912,13 @@ ${e}
 允许任意IP
 签到`},{icon:"",title:"Elysia-备用",link:"https://elysia.h-e.top",text:`https://linux.do/t/topic/1175087
 优化线路 允许任意IP RPM稍低
-签到`},{icon:"",title:"Mu.Li API Relive",link:"https://demo.awa1.fun/",text:`https://linux.do/t/topic/927953/177
-用完了找楼主要`},{icon:"",title:"colin1112",link:"https://api.colin1112.me/",text:`https://linux.do/t/topic/977702
-抽奖`},{icon:"",title:"小辣椒",link:"https://yyds.215.im/",text:"确实辣"},{icon:"",title:"薄荷AI",link:"https://x666.me/",text:`薄荷AI,无须多言
+签到`},{icon:"",title:"小辣椒",link:"https://yyds.215.im/",text:"确实辣"},{icon:"",title:"薄荷AI",link:"https://x666.me/",text:`薄荷AI,无须多言
 抽奖`},{icon:"",title:"AnAnAPI",link:"https://www.ananapi.com/",text:`https://linux.do/t/topic/1828940
-AnAnAPI，爱您`},{icon:"",title:"Token Atlas",link:"https://gptfreetoken.pony.indevs.in",text:`https://linux.do/t/topic/1755909
-只领取token`},{icon:"",title:"520china",link:"https://aidrouter.qzz.io/",text:`https://linux.do/t/topic/1371565/69
-520china`},{icon:"",title:"97",link:"https://free.9977.me/",text:"一天20刀"},{icon:"",title:"luciferai",link:"https://free.luciferai.cc/",text:""},{icon:"",title:"冰！",link:"https://ice.v.ua/",text:"竟然是冰！重置200"},{icon:"",title:"bwen",link:"https://api.bwen.net/",text:"竟然是bwen!"},{icon:"",title:"dev88",link:"https://api.dev88.tech/",text:`https://linux.do/t/topic/1101573
+AnAnAPI，爱您`},{icon:"",title:"luciferai",link:"https://free.luciferai.cc/",text:""},{icon:"",title:"冰！",link:"https://icoe.pp.ua/",text:"竟然是冰！重置200"},{icon:"",title:"bwen",link:"https://api.bwen.net/",text:"竟然是bwen!"},{icon:"",title:"dev88",link:"https://api.dev88.tech/",text:`https://linux.do/t/topic/1101573
 每天自動檢查剩餘額度 餘額低於$30會補充$10直到签到出现`},{icon:"",title:"🍌 nano-banana",link:"https://nano-banana.dev88.me/",text:"👈dev88的大香蕉"},{icon:"",title:"Huan API",link:"https://ai.huan666.de/",text:`和阿焕有友联了
-签到`},{icon:"",title:"Pond hub",link:"https://code.claudex.us.ci/",text:"PondPondPond"},{icon:"",title:"Ketchup AI - Free Unlimited AI Image Generator",link:"https://ketchup-ai.com/",text:`无限的图片生成！
+签到`},{icon:"",title:"Ketchup AI - Free Unlimited AI Image Generator",link:"https://ketchup-ai.com/",text:`无限的图片生成！
 那还说啥了，🍅🐮🍺`},{icon:"",title:"包子铺-Donehub",link:"https://api.5202030.xyz/",text:`包子铺的Donehub
-好像渠道快死完了，白嫖这么久，等发工资了投喂一下`},{icon:"",title:"包子铺-NewApi",link:"https://api.codeqaq.com/",text:"包子铺的Newapi"},{icon:"",title:"KFC API",link:"https://kfc-api.sxxe.net/login?expired=true",text:"每周四开放"},{icon:"",title:"莹のAPI",link:"https://api.wpgzs.top",text:`KYX继承者
-抽奖`},{icon:"",title:"SWT-API",link:"https://api.lhyb.dpdns.org/pricing",text:`https://linux.do/t/topic/1106811
+好像渠道快死完了，白嫖这么久，等发工资了投喂一下`},{icon:"",title:"包子铺-NewApi",link:"https://api.codeqaq.com/",text:"包子铺的Newapi"},{icon:"",title:"SWT-API",link:"https://api.lhyb.dpdns.org/pricing",text:`https://linux.do/t/topic/1106811
 都不知道咋说了，总是是白嫖大王`},{icon:"",title:"SWT-API-备用",link:"https://api2.lhyb.dpdns.org/",text:"SWT-API"},{icon:"",title:"SWT-API-备用2",link:"https://new-api.koyeb.app",text:"SWT-API"},{icon:"",title:"黑与白",link:"https://ai.hybgzs.com/panel",text:`https://linux.do/t/topic/1084752
 欧阳淇淇
 抽奖`},{icon:"",title:"Agent Router",link:"https://agentrouter.org/",text:"是Neo吗？"},{icon:"",title:"SoraApi",link:"https://api.67.si/",text:`https://linux.do/t/topic/1111114
